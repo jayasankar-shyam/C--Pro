727f80c7c122d40646fe829e7e5f60786e759971
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-09-13 20:55:35 +0530

    fact

--- recursive_fn.c
+++ recursive_fn.c
@@ -1,10 +1,14 @@
+/*Jayasankar Shyam
+S2 AD
+Roll No : 40*/
 #include <stdio.h>
 long fact(int);
 int main()
 {
     int n;
+    printf("Enter the number : ");
     scanf("%d",&n);
-    printf("%ld",fact(n));
+    printf("\nThe factorial of %d is %ld \n",n,fact(n));
     return 0;
 }
 long fact(int n)
@@ -14,3 +18,4 @@ long fact(int n)
     else
         return n*fact(n-1);
 }
+
