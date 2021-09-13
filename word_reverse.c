3d05d24ce5ecbfb518126d7ff9301e3c1ea084f1
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-09-13 22:54:10 +0530

    reverse_word

---
+++ word_reverse.c
@@ -0,0 +1,21 @@
+/*Jayasankar Shyam
+S2 AD
+Roll No : 40*/
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int length,i;
+    char temp, a[100];
+    printf("Enter the word :");
+    gets(a);
+    length=strlen(a)-1;
+    for(i=0;i<=length/2;i++)
+    {
+        temp=a[i];
+        a[i]=a[length-i];
+        a[length-i]=temp;
+    }
+    printf("The reversed word is %s ",a);
+    return 0;
+}
