d4aa135cdd58a9a740cda5261005022aab9ce54d
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-09-14 00:26:36 +0530

    rev

--- word_reverse.c
+++ word_reverse.c
@@ -1,21 +1,29 @@
 /*Jayasankar Shyam
 S2 AD
 Roll No : 40*/
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+char reverse(char a[]);
 int main()
 {
-    int length,i;
-    char temp, a[100];
+    char a[100];
     printf("Enter the word :");
     gets(a);
-    length=strlen(a)-1;
-    for(i=0;i<=length/2;i++)
-    {
-        temp=a[i];
-        a[i]=a[length-i];
-        a[length-i]=temp;
-    }
-    printf("The reversed word is %s ",a);
+    reverse(a);
+    printf("\nThe reversed word is %s \n", a);
+
     return 0;
+}
+char reverse(char a[])
+{
+    int length, i;
+    char temp;
+    length = strlen(a) - 1;
+    for (i = 0; i <= length / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[length - i];
+        a[length - i] = temp;
+    }
+    return a;
 }
