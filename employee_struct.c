6195cfc24344832dc26b6db0f63dcc11317984b8
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-09-18 09:24:41 +0530

    n employee info

--- employee_struct.c
+++ employee_struct.c
@@ -1,3 +1,6 @@
+/*  JAYASANKAR SHYAM
+    S2 AD
+    ROLL NO : 40*/
 #include <stdio.h>
 int main()
 {
@@ -5,29 +8,32 @@ int main()
     struct employee
     {
         char name[50];
-        int age;
+        int employee_id;
         float salary;
     };
-    struct employee emp[10];
+    struct employee emp[50];
     printf("Enter the number of employees : ");
     scanf("%d", &n);
+    printf("\n");
     for (i = 0; i < n; i++)
     {
-        
-        printf("Enter Full name : ");
+
+        printf("Enter the name : ");
         scanf("%s",&emp[i].name);
-        printf("Enter age : ");
-        scanf("%d", &emp[i].age);
+        printf("Enter employee id : ");
+        scanf("%d", &emp[i].employee_id);
         printf("Enter salary : ");
         scanf("%f", &emp[i].salary);
         printf("\n");
     }
-    printf("\nDisplaying Information\n");
+    printf("\n------------------Displaying Information------------------\n");
     for (i = 0; i < n; i++)
     {
+        printf("Employee No - %d\n",i+1);
         printf("Name : %s\n", emp[i].name);
-        printf("Age : %d\n", emp[i].age);
+        printf("Employee Id : %d\n", emp[i].employee_id);
         printf("Salary : %.2f\n", emp[i].salary);
+        printf("\n");
     }
     return 0;
 }
