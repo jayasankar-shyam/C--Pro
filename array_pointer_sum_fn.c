717dfbd25065a67e0b1e7cfdbe521df46d1323f0
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-09-23 23:34:22 +0530

    sum

--- array_pointer_sum_fn.c
+++ array_pointer_sum_fn.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+int sum(int *x, int n);
 int main()
 {
-    
+    int a[100], i, n, s = 0;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("\nEnter the elements:");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", (a + i));
+    }
+    printf("Sum of array elements = %d ",sum(a,n));
+}
+int sum(int *x, int n)
+{
+    int i, sum = 0;
+    for (i = 0; i < n; i++)
+        sum += *(x + i);
+    return sum;
 }
